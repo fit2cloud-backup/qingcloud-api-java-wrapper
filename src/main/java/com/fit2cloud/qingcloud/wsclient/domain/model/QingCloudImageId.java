6a03e5c34b087cf76aacb83745b69c364b051878
Jason <jason@fit2cloud.com>
2014-08-31 23:44:30 +0800

    add image ids.

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudImageId.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudImageId.java
@@ -2,4 +2,11 @@ package com.fit2cloud.qingcloud.wsclient.domain.model;
 
 public class QingCloudImageId {
     public final static String CENTOS64_X86a = "centos64x86a";
+    public final static String CENTOS63_X64a = "centos63x64a";
+    public final static String CENTOS64_X64a = "centos64x64b";
+    public final static String CENTOS65_X64b = "centos65x64b";
+    public final static String UBUNTU_1204_2_LTS_64 = "precisex64c";
+    public final static String UBUNTU_1210_64 = "quantalx64b";
+    public final static String UBUNTU_1310_64 = "saucysrvx64b";
+    public final static String UBUNTU_1404_LTS_64 = "trustysrvx64a";
 }
