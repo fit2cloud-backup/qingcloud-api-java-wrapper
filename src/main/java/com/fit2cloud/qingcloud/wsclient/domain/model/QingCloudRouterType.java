a307d456be77d710b7f6639caab6efe500f0b934
Fit2Cloud Team <fittocloud@gmail.com>
2014-09-10 16:53:08 +0800

    update

---
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudRouterType.java
@@ -0,0 +1,7 @@
+package com.fit2cloud.qingcloud.wsclient.domain.model;
+
+public class QingCloudRouterType {
+	public static final Integer SMALL = 1;
+	public static final Integer MEDIUM = 0;
+	public static final Integer LARGE = 2;
+}
