829b872a2b3343baa412431c11e9500fa33a515c
Fit2Cloud Team <fittocloud@gmail.com>
2014-09-19 17:56:11 +0800

    add eip status class

---
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudEIPStatus.java
@@ -0,0 +1,9 @@
+package com.fit2cloud.qingcloud.wsclient.domain.model;
+public class QingCloudEIPStatus {
+	public static final String PENDING = "pending";
+	public static final String AVAILABLE = "available";
+	public static final String ASSOCIATED = "associated";
+	public static final String SUSPENDED = "suspended";
+	public static final String RELEASED = "released";
+	public static final String CEASED = "ceased";
+}
