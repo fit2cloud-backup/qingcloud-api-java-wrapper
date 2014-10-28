110656ddd1b478b96c343269f9d7ba55dee7762b
Fit2Cloud Team <fittocloud@gmail.com>
2014-10-29 01:02:43 +0800

    增加亚太1区

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZone.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZone.java
@@ -4,4 +4,5 @@ public class QingCloudZone {
 	public final static String PEK1 = "pek1";
     public final static String PEK2 = "pek2";
     public final static String GD1 = "gd1";
+    public final static String AP1 = "ap1";
 }
