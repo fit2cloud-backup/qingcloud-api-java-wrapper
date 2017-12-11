cdf02141d7b1c811d3854ada8587267fda3d513e
shenkaibo <kaibo@fit2cloud.com>
2017-12-11 16:01:36 +0800

    [update]修改青云磁盘 type elem

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVolumeType.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVolumeType.java
@@ -2,5 +2,8 @@ package com.fit2cloud.qingcloud.wsclient.domain.model;
 
 public class QingCloudVolumeType {
 	public final static int PERFORMANCE = 0;
-	public final static int CAPACITY = 1;
+	public final static int CAPACITY_1 = 1;
+	public final static int CAPACITY_2 = 2;
+	public final static int HIGH_PERFORMANCE = 3;
+
 }
