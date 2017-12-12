36eeaef21d1167effacbf459796962f021cfa2ea
wangzhengkun <zhengkun@fit2cloud.com>
2017-12-12 10:40:59 +0800

    增加宿主机状态 down

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudBotStatus.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudBotStatus.java
@@ -8,5 +8,6 @@ public class QingCloudBotStatus {
     public static final String ACTIVE="active"; //可用状态，正常分配资源
     public static final String FAULTY="faulty"; //故障
     public static final String RESCUING="rescuing"; //修复中
+    public static final String DOWN="down";//设备下线 （文档里没写 坑死我了）
 
 }
