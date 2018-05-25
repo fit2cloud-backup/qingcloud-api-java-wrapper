6ab92b68606f87589e25f19aeb5113c06ec8372e
xujian <xujian@fit2cloud.com>
2018-05-25 11:36:50 +0800

    Feature #1187 [山西农信] 新建安全组能加IP端口集合 更新： 获取青云IP/端口集合接口，补追参数id

--- src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeSecurityGroupIPSetsRequest.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeSecurityGroupIPSetsRequest.java
@@ -1,8 +1,18 @@
 package com.fit2cloud.qingcloud.wsclient.ui.model;
 
 
+import java.util.List;
+
 public class DescribeSecurityGroupIPSetsRequest extends Request {
     private String zone;
+
+    private Integer ipset_type;
+
+    private List<String> security_group_ipsets;
+
+
+    public DescribeSecurityGroupIPSetsRequest() {
+    }
 
     public String getZone() {
         return zone;
@@ -11,4 +21,21 @@ public class DescribeSecurityGroupIPSetsRequest extends Request {
     public void setZone(String zone) {
         this.zone = zone;
     }
+
+    public Integer getIpset_type() {
+        return ipset_type;
+    }
+
+    public void setIpset_type(Integer ipset_type) {
+        this.ipset_type = ipset_type;
+    }
+
+    public List<String> getSecurity_group_ipsets() {
+        return security_group_ipsets;
+    }
+
+    public void setSecurity_group_ipsets(List<String> security_group_ipsets) {
+        this.security_group_ipsets = security_group_ipsets;
+    }
+
 }
