dd86cb76bb593c8678d64bebdba1be5a08f40c91
”yangxu-fit2cloud” <xu.yang@fit2cloud.com>
2019-07-23 17:21:21 +0800

    杨旭  修改  获取子网信息返回值增加

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVxnetDetail.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudVxnetDetail.java
@@ -11,6 +11,9 @@ public class QingCloudVxnetDetail {
     private String description;
     private List<String> instance_ids;
     private QingcloudRouter router;
+	private String network_acl_id;
+	private String vpc_router_id;
+
 	public String getVxnet_name() {
 		return vxnet_name;
 	}
@@ -60,5 +63,21 @@ public class QingCloudVxnetDetail {
 				+ ", create_time=" + create_time + ", description="
 				+ description + ", instance_ids=" + instance_ids + ", router="
 				+ router + "]";
+	}
+
+	public String getNetwork_acl_id() {
+		return network_acl_id;
+	}
+
+	public void setNetwork_acl_id(String network_acl_id) {
+		this.network_acl_id = network_acl_id;
+	}
+
+	public String getVpc_router_id() {
+		return vpc_router_id;
+	}
+
+	public void setVpc_router_id(String vpc_router_id) {
+		this.vpc_router_id = vpc_router_id;
 	}
 }
