bda20d2e8642f56b896b67c6273d54ed9dd3f12b
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2020-02-13 17:58:44 +0800

    添加同步快照(备份)、根据备份创建镜像

--- src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/CaptureInstanceFromSnapshotRequest.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/CaptureInstanceFromSnapshotRequest.java
@@ -1,9 +1,53 @@
 package com.fit2cloud.qingcloud.wsclient.ui.model;
 
-import java.util.List;
-
-
 public class CaptureInstanceFromSnapshotRequest extends Request {
     
+    /**
+     * 备份ID
+     */
+    private String snapshot;
+    /**
+     * 镜像名称
+     */
+    private String image_name;
+    /**
+     * 用户Id
+     */
+    private String owner;
+    /**
+     * 区域
+     */
+    private String zone;
 
+    public String getSnapshot() {
+        return snapshot;
+    }
+
+    public void setSnapshot(String snapshot) {
+        this.snapshot = snapshot;
+    }
+
+    public String getImage_name() {
+        return image_name;
+    }
+
+    public void setImage_name(String image_name) {
+        this.image_name = image_name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
 }
