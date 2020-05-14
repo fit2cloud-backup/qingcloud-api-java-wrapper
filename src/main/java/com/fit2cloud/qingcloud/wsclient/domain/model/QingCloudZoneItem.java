3865f9fb025e7d3fdef180bb3403c0c309d28862
houchen <chen.hou@fit2cloud.com>
2020-05-14 09:39:40 +0800

    添加zoneName字段

--- src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZoneItem.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/domain/model/QingCloudZoneItem.java
@@ -2,6 +2,7 @@ package com.fit2cloud.qingcloud.wsclient.domain.model;
 
 public class QingCloudZoneItem {
 	private String zone_id;
+	private String zoneName;
 	private String status;
 	public String getZone_id() {
 		return zone_id;
@@ -9,6 +10,15 @@ public class QingCloudZoneItem {
 	public void setZone_id(String zone_id) {
 		this.zone_id = zone_id;
 	}
+
+	public String getZoneName() {
+		return zoneName;
+	}
+
+	public void setZoneName(String zoneName) {
+		this.zoneName = zoneName;
+	}
+
 	public String getStatus() {
 		return status;
 	}
