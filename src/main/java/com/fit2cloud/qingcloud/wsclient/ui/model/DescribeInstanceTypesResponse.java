b1c5432e0daf3379a7502990d7a35454367127ad
zhangchaochao-fit2cloud <chaochao.zhang@fit2cloud.com>
2021-05-24 14:36:33 +0800

    fix: 青云API字段变更，添加适配方法

--- src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeInstanceTypesResponse.java
+++ src/main/java/com/fit2cloud/qingcloud/wsclient/ui/model/DescribeInstanceTypesResponse.java
@@ -1,6 +1,7 @@
 package com.fit2cloud.qingcloud.wsclient.ui.model;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public class DescribeInstanceTypesResponse {
 
     public static DescribeInstanceTypesResponse fromJson(String jsonDescribeInstanceTypesResponse){
         Gson gson = new Gson();
-        DescribeInstanceTypesResponse describeInstanceTypesResponse = gson.fromJson(jsonDescribeInstanceTypesResponse, DescribeInstanceTypesResponse.class);
+        DescribeInstanceTypesResponse describeInstanceTypesResponse = gson.fromJson(convertResponse(jsonDescribeInstanceTypesResponse), DescribeInstanceTypesResponse.class);
         return describeInstanceTypesResponse;
     }
 
@@ -37,5 +38,27 @@ public class DescribeInstanceTypesResponse {
 
     public void setInstance_type_set(List<QingCloudInstanceType> instance_type_set) {
         this.instance_type_set = instance_type_set;
+    }
+
+    /**
+     * 青云API变更，通过方法转换
+     * @param jsonDescribeInstanceTypesResponse 转换值
+     * @return 转换后的JsonObject
+     */
+    private static JsonObject convertResponse (String jsonDescribeInstanceTypesResponse) {
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObj = parser.parse(jsonDescribeInstanceTypesResponse).getAsJsonObject();
+        if (jsonObj.get("total_count").isJsonArray()) {
+            int totalCount = 0;
+            JsonArray totalCountArray = jsonObj.getAsJsonArray("total_count");
+            jsonObj.remove("total_count");
+            for (JsonElement countEle: totalCountArray) {
+                JsonObject asJsonObject = countEle.getAsJsonObject();
+                int count = asJsonObject.get("count").getAsInt();
+                totalCount += count;
+            }
+            jsonObj.add("total_count", new Gson().toJsonTree(totalCount));
+        }
+        return jsonObj;
     }
 }
